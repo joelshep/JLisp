1c3576c6d7eb6cae8ccda57ae181d3908b0088bb
Joel Shepherd <joelshep@ix.netcom.com>
2023-07-21 12:52:39 -0700

    Adding CollectionUtils

---
+++ src/org/ulithi/jlisp/commons/CollectionUtils.java
@@ -0,0 +1,18 @@
+package org.ulithi.jlisp.commons;
+
+import java.util.Collection;
+
+/**
+ * A few static methods to simplify working with {@link Collection} objects.
+ */
+public class CollectionUtils {
+
+    /**
+     * Indicates if the given {@link Collection} is null or empty.
+     * @param collection A {@link Collection} object.
+     * @return True if the {@link Collection} is not null and not empty, false otherwise.
+     */
+    public static <T> boolean isEmpty(final Collection<T> collection) {
+        return (collection == null || collection.isEmpty());
+    }
+}
