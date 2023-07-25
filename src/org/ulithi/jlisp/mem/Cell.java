ec3311e29aabe0ab8f1c2b157a9aa0c83d15e7fd
Joel Shepherd <joelshep@ix.netcom.com>
2023-07-25 08:10:23 -0700

    Removing CAR/CDR concept; replacing with more basic first/rest references, to more clearly separate the language and memory models

--- src/org/ulithi/jlisp/mem/Cell.java
+++ src/org/ulithi/jlisp/mem/Cell.java
@@ -5,73 +5,129 @@ import org.apache.commons.lang3.Validate;
 import static org.ulithi.jlisp.parser.Grammar.*;
 
 /**
- * A {@link Cell} -- a.k.a. a {@code cons} in LISP terminology -- is a pair of {@link SExpression}
- * references: {@code car} (or {@code left}) and {@code cdr} (or {@code right}). Both references
- * are <em>required</em>, however the {@code cdr} reference can be the special {@code NIL} value.
+ * A {@link Cell} is a pair of {@link Ref references}: {@code first} (or {@code left}) and
+ * {@code rest} (or {@code right}). Both references are <em>required</em>, however the
+ * {@code rest} reference can be the special {@code NIL} value. A reference can refer to an
+ * {@link Atom} or another {@code Cell}.
+ * <p>
+ * Cells are part of JLISP's <em>memory model</em>, not part of the language model. While it seemed
+ * natural for cell fields to be treated as CAR and CDR, they aren't quite the same. E.g., the
+ * CDR of (2 (1 2 3)) is (1 2 3): a list. But the in-memory representation is (2 . ((1 2 3) . NIL)):
+ * the "CDR" of that is a <em>cell</em>, not a list.
+ * <p>
+ * So, within a cell -- a dotted-pair -- the first/left-hand field is called {@code first} and the
+ * second/right-hand field is called {@code rest}. When callers fetch these fields, they receive
+ * {@code Refs}, which they then need to handle appropriately, depending on whether the reference
+ * is to an {@code Atom} or another {@code Cell}.
  */
-public class Cell implements SExpression {
+public class Cell implements Ref {
     /**
-     * The car/first/lhs SExpression in this cell.
+     * The first/lhs field in this cell. This can be an Atom, or a reference to a Cell.
      */
-    private SExpression car;
+    private Ref first;
 
     /**
-     * The cdr/second/rhs SExpression in this cell.
+     * The second/rhs field in this cell. This is typically either a reference to the next
+     * Cell, or NIL.
      */
-    private SExpression cdr;
+    private Ref rest;
 
     /**
      * Private constructor. Use one of the {@code create} methods to create a new Cell.
-     * @param car The new Cell's {@code car} element.
-     * @param cdr The new Cell's {@code cdr} element.
+     * @param first The new Cell's {@code first} element.
+     * @param rest The new Cell's {@code rest} element.
      */
-    private Cell(final SExpression car, final SExpression cdr) {
-        Validate.notNull(car);
-        Validate.notNull(cdr);
-        this.car = car;
-        this.cdr = cdr;
+    private Cell(final Ref first, final Ref rest) {
+        Validate.notNull(first);
+        Validate.notNull(rest);
+        this.first = first;
+        this.rest = rest;
     }
 
     /**
      * Constructs a new {@link Cell} with a literal {@link Atom} for the given {@code token} as
-     * the first element and NIL for the rest element.
+     * the {@code first} element and {@code NIL} as the {@code rest} element.
      *
      * @param token A literal used to construct the Atom for the new cell's first element.
-     * @return The new {@code Cell} of the form (ATOM . NIL).
+     * @return A new {@code Cell} of the form (ATOM . NIL).
      */
     public static Cell create(final String token) {
         return create(Atom.create(token));
     }
 
+    /**
+     * Constructs a new {@link Cell} with the given {@link Atom} as the {@code first} element and
+     * {@code NIL} as the {@code rest} element.
+     * @param atom The {@code Atom} that will be the new cell's {@code first} element.
+     * @return A new {@code Cell} of the form (ATOM . NIL).
+     */
     public static Cell create(final Atom atom) {
         return new Cell(atom, Atom.NIL);
     }
 
     /**
-     * Constructs a new {@link Cell} with a {@code Cell} (e.g., representing a list) as the
-     * first element and NIL for the rest element.
-     *
-     * @param cell A {@link Cell} representing a {@code cons}.
-     * @return The new {@code Cell} of the form (CELL . NIL)
+     * Constructs a new {@link Cell} with the given {@code Cell} -- assumed to be the root cell
+     * of a list -- as the {@code first} element and {@code NIL} as the {@code rest} element.
+     * @param cell The root cell of a list.
+     * @return A new {@code Cell} of the form (ROOT_CELL . NIL).
      */
-    public static Cell create(final Cell cell) {
+    public static Cell createAsList(final Cell cell) {
         return new Cell(cell, Atom.NIL);
     }
 
+    /**
+     * Creates a new {@link Cell} with {@code NIL} as both its {@code first} and {@code rest}
+     * fields. Such a cell isn't valid (I think) as a LISP construct, but as a memory construct
+     * it is useful for initializing the equivalent of a null {@code Cell} reference.
+     * @return A new {@code Cell} with the {@code NIL} value for both fields.
+     */
     public static Cell create() {
         return new Cell(Atom.NIL, Atom.NIL);
     }
 
-    public boolean isNil() {
-        return (car.equals(Atom.NIL) && cdr.equals(Atom.NIL));
+    /**
+     * A {@link Cell} itself is not an {@code Atom}, although its elements may be.
+     * @return False.
+     */
+    @Override
+    public boolean isAtom() {
+        return false;
     }
 
     /**
-     * Returns the {@code car} (first/lhs) element of this {@link Cell}.
-     * @return The {@code car} (first/lhs) element of this {@code Cell}.
+     * Indicates if this {@link Cell Cell's} {@code first} element is an {@link List}.
+     * @return True if this {@code Cell's} {@code first} element is an {{@code List}, false
+     *         otherwise.
      */
-    public SExpression getCar() {
-        return this.car;
+    @Override
+    public boolean isList() {
+        return first.isList();
+    }
+
+    /**
+     * Indicates if this {@link Cell} is the special "NIL" cell.
+     * @return True if this {@code Cell} is a "NIL cell", false otherwise.
+     */
+    @Override
+    public boolean isNil() {
+        return (first.equals(Atom.NIL) && rest.equals(Atom.NIL));
+    }
+
+    /**
+     * Indicates if this {@link Cell} is a {@code Cell}. It is.
+     * @return True.
+     */
+    @Override
+    public boolean isCell() {
+        return true;
+    }
+
+    /**
+     * Returns the {@code first} field of this {@link Cell} as a {@link Ref}.
+     * @return The {@code first} field of this {@code Cell}.
+     */
+    public Ref getFirst() {
+        return this.first;
     }
 
     /**
@@ -79,27 +135,28 @@ public class Cell implements SExpression {
      * or List represented by the given {@link SExpression}.
      * @param sexp A non-null {@link SExpression}.
      */
-    public void setCar(final SExpression sexp) {
+    @Deprecated
+    public void setFirst(final Ref sexp) {
         Validate.notNull(sexp);
-        this.car = sexp;
+        this.first = sexp;
     }
 
     /**
-     * Returns the {@code cdr} (second/rhs) element of this {@link Cell}.
-     * @return The {@code cdr} element of this {@code Cell}.
+     * Returns the {@code rest} field of this {@link Cell} as a {@link Ref}.
+     * @return The {@code rest} field of this {@code Cell}.
      */
-    public SExpression getCdr() {
-        return this.cdr;
+    public Ref getRest() {
+        return this.rest;
     }
 
     /**
-     * Sets the {@code cdr} (second/rhs element of this {@link} cell) to the {@link Atom}, Symbol
-     * or List represented by the given {@link SExpression}.
-     * @param sexp A non-null {@link SExpression}.
+     * Sets the {@code rest} field of this {@link} cell to the {@link Atom}, Symbol
+     * or List represented by the given {@link Ref}.
+     * @param ref A non-null {@link Ref}.
      */
-    public void setCdr(final SExpression sexp) {
-        Validate.notNull(sexp);
-        this.cdr = sexp;
+    public void setRest(final Ref ref) {
+        Validate.notNull(ref);
+        this.rest = ref;
     }
 
     /**
@@ -109,11 +166,11 @@ public class Cell implements SExpression {
     @Override
     public String toString() {
         return LPAREN +
-               car +
+               first +
                SPACE +
                DOT +
                SPACE +
-               cdr +
+               rest +
                RPAREN;
     }
 }
