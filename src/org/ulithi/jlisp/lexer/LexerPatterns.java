107cd23ccc547ab39087a17ed2758bf52158e6cc
Joel Shepherd <joelshep@ix.netcom.com>
2023-07-25 07:49:09 -0700

    Extended pattern for Symbol. This may be temporary.

--- src/org/ulithi/jlisp/lexer/LexerPatterns.java
+++ src/org/ulithi/jlisp/lexer/LexerPatterns.java
@@ -3,11 +3,13 @@ package org.ulithi.jlisp.lexer;
 /**
  * Regular expressions which represent meaningful and acceptable
  * strings in a Lisp program.
+ * <p>
+ * TODO - Merge this with Grammar.
  */
 class LexerPatterns {
 	public static final String LETTER = "[a-zA-Z]";
 	public static final String LITERAL = "[a-zA-Z0-9]+";
 	public static final String WHITESPACE = "[\\s]+";
 	public static final String NUMERIC_ATOM = "[\\d\\+\\-]?[\\d]*";
-	public static final String SYMBOL = "[().]";
+	public static final String SYMBOL = "[().\\+\\*]";
 }
