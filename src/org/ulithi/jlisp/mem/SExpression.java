c97482a514ec61075cf2b87d4d965b914c61ff38
Joel Shepherd <joelshep@ix.netcom.com>
2023-07-25 07:59:19 -0700

    Adding boolean type-check methods

--- src/org/ulithi/jlisp/mem/SExpression.java
+++ src/org/ulithi/jlisp/mem/SExpression.java
@@ -3,6 +3,12 @@ package org.ulithi.jlisp.mem;
 /**
  * An {@link SExpression} is the fundamental LISP language element, the basis for both {@link Atom Atoms}
  * (literals and symbols) and lists.
+ * @deprecated I believe this interface may be redundant with the {@link Ref} interface, which is
+ * better defined.
  */
+@Deprecated()
 public interface SExpression {
+    boolean isList();
+    boolean isNil();
+    boolean isAtom();
 }
