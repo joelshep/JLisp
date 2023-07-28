592dfcefc77e19ae13f114f23b17af5f384aef30
Joel Shepherd <joelshep@ix.netcom.com>
2023-07-27 18:42:13 -0700

    Adding Javadoc

--- src/org/ulithi/jlisp/core/Interpreter.java
+++ src/org/ulithi/jlisp/core/Interpreter.java
@@ -6,26 +6,55 @@ import org.ulithi.jlisp.parser.Environment;
 import org.ulithi.jlisp.parser.Parser;
 import org.ulithi.jlisp.primitive.Eval;
 
-import java.io.InputStream;
-
+/**
+ * The top-level JLISP interpreter. Feed it a LISP expression, and it will scan, parse and
+ * evaluate it. Repeat as necessary. Runs on its own thread to stabilize integration with the
+ * main client application (e.g. a console REPL-style app).
+ */
 public class Interpreter implements Runnable {
-
+    /**
+     * The LISP environment: Binds symbols, variables, function names, etc. to their storage or
+     * definitions. At some point, the environment may be lexically scoped (allowing the same
+     * name to refer to different things in different lexical scopes) but for now all environment
+     * entries are global.
+     */
     private final Environment environment;
 
+    /**
+     * The eval function instance used by this interpreter.
+     */
     private final Eval eval = new Eval();
 
+    /**
+     * Creates a new {@link Interpreter} and its {@link Environment}.
+     */
     public Interpreter () {
         this.environment = new Environment();
     }
 
-    public void initialize() {
+    /**
+     * Initializes this {@link Interpreter}.
+     */
+    public void initialize() { }
 
-    }
+    /**
+     * Resets this {@link Interpreter} (allowing continued operation after an expression
+     * evaluation failure.
+     */
+    public void reset() { }
 
-    public void reset() {
+    /**
+     * Invoked when the {@link Interpreter} execution is initiated on its own thread.
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() { }
 
-    }
-
+    /**
+     * Initiates the scanning, parsing and evaluation of the given LISP expression.
+     * @param expression A LISP expression to evaluate.
+     * @return True if expression was evaluated successfully, false otherwise.
+     */
     public boolean offer(final String expression) {
         try {
             final Lexer lexer = new Lexer(expression);
@@ -38,10 +67,5 @@ public class Interpreter implements Runnable {
             System.err.println(e.getMessage());
             return false;
         }
-    }
-
-    @Override
-    public void run() {
-
     }
 }
