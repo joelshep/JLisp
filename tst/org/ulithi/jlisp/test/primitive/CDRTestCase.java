c6257860e521b6630b241131de93d9460a23370e
Joel Shepherd <joelshep@ix.netcom.com>
2023-08-02 21:23:53 -0700

    Adding failure case for CDR of a literal

--- tst/org/ulithi/jlisp/test/primitive/CDRTestCase.java
+++ tst/org/ulithi/jlisp/test/primitive/CDRTestCase.java
@@ -4,20 +4,28 @@ import org.junit.Test;
 import org.ulithi.jlisp.core.Atom;
 import org.ulithi.jlisp.core.List;
 import org.ulithi.jlisp.core.SExpression;
+import org.ulithi.jlisp.exception.EvaluationException;
 import org.ulithi.jlisp.mem.PTree;
 import org.ulithi.jlisp.primitive.CAR;
 import org.ulithi.jlisp.primitive.CDR;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.ulithi.jlisp.test.suite.UnitTestUtilities.parse;
 
 public class CDRTestCase {
+
     @Test
-    public void testCDROfSimpleList() {
-        final String expression = "(4 5 6)";
+    public void testCDROfLiteral() {
+        final String expression = "HELLO";
         final PTree pTree = parse(expression);
-        final List list = (List)(new CDR()).apply(SExpression.create(pTree.root()));
-        assertEquals("(5 . (6 . NIL))", String.valueOf(list));
+
+        try {
+            final SExpression sexp = (new CDR()).apply(SExpression.create(pTree.root()));
+            fail("CAR should throw exception if argument is not a list");
+        } catch (final EvaluationException e) {
+            // Expected.
+        }
     }
 
     @Test
@@ -29,8 +37,16 @@ public class CDRTestCase {
     }
 
     @Test
+    public void testCDROfSimpleList() {
+        final String expression = "(4 5 6)";
+        final PTree pTree = parse(expression);
+        final List list = (List)(new CDR()).apply(SExpression.create(pTree.root()));
+        assertEquals("(5 . (6 . NIL))", String.valueOf(list));
+    }
+
+    @Test
     public void testCDROfListOfLists() {
-        final String expression = "((A B) (C D) (E F)))";
+        final String expression = "((A B) (C D) (E F))";
         final PTree pTree = parse(expression);
         final List list = (List)(new CDR()).apply(SExpression.create(pTree.root()));
         assertEquals("((C . (D . NIL)) . ((E . (F . NIL)) . NIL))", String.valueOf(list));
