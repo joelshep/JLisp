944ee02e543a4869c5c9aefe0a549f942315a73e
Joel Shepherd <joelshep@ix.netcom.com>
2023-08-29 21:46:59 -0700

    Adding exception for when a function is invoked with the wrong number of arguments

--- src/org/ulithi/jlisp/exception/WrongArgumentCountException.java
+++ src/org/ulithi/jlisp/exception/WrongArgumentCountException.java
@@ -1,2 +1,10 @@
-package org.ulithi.jlisp.exception;public class WrongArgumentCountException {
+package org.ulithi.jlisp.exception;
+
+/**
+ * Thrown when a function is invoked with the wrong number of arguments.
+ */
+public class WrongArgumentCountException extends JLispRuntimeException {
+    public WrongArgumentCountException(final String message) {
+        super(message);
+    }
 }
