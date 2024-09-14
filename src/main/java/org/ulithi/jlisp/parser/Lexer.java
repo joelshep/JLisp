becb4d213a6b4a1d1322943aa2829cb10881567f
Joel Shepherd <joelshep@ix.netcom.com>
2024-09-13 22:46:42 -0700

    Minor lexer improvements: removing state, checking result of stream read, etc,

--- src/main/java/org/ulithi/jlisp/parser/Lexer.java
+++ src/main/java/org/ulithi/jlisp/parser/Lexer.java
@@ -3,6 +3,7 @@ package org.ulithi.jlisp.parser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,76 +12,74 @@ import java.util.List;
  * characters representing a LISP program or expression to be evaluated.
  */
 public class Lexer {
-	/**
-	 * The characters to be tokenized.
-	 */
-	private String text = "";
 
-	/**
-	 * The ordered list of tokens produced by lexical analysis.
-	 */
-	private final List<String> tokens;
+    /**
+     * The ordered list of tokens produced by lexical analysis.
+     */
+    private final List<String> tokens;
 
-	/**
-	 * Constructor which can take a stream as input. It reads the stream and tokenizes
-	 * it appropriately.
-	 *
-	 * @param stream An input stream
-	 */
-	public Lexer(final InputStream stream) throws IOException {
-		final byte[] bytes = new byte[1024];
-		while (stream.available() > 0) {
-	        stream.read(bytes, 0, 1024);
-	        text = text.concat(new String(bytes)).trim().toUpperCase();
+    /**
+     * Constructor which can take a stream as input. It reads the stream and tokenizes
+     * it appropriately.
+     *
+     * @param stream An input stream
+     */
+    public Lexer(final InputStream stream) throws IOException {
+        final byte[] bytes = new byte[1024];
+        final StringBuilder text = new StringBuilder();
+
+        while (stream.available() > 0 && stream.read(bytes, 0, 1024) > 0) {
+            text.append(new String(bytes).trim().toUpperCase());
+            Arrays.fill(bytes, (byte) 0);
         }
 
-        tokens = tokenize(text);
-	}
+        tokens = tokenize(text.toString());
+    }
 
-	/**
-	 * Constructs a {@link Lexer} to produce tokens from a given input string.
-	 * @param s A string -- presumably representing a LISP program or expression -- to be tokenized.
-	 */
-	public Lexer(final String s) {
-		text = s;
-		tokens = tokenize(text);
-	}
+    /**
+     * Constructs a {@link Lexer} to produce tokens from a given input string.
+     * @param s A string -- presumably representing a LISP program or expression -- to be tokenized.
+     */
+    public Lexer(final String s) {
+        tokens = tokenize(s);
+    }
 
-	/**
-	 * Returns the tokens formed from the stream or string that this {@link Lexer} parsed.
-	 *
-	 * @return An ordered list of tokens.
-	 */
-	public List<String> getTokens() {
-		return Collections.unmodifiableList(tokens);
-	}
+    /**
+     * Returns the tokens formed from the stream or string that this {@link Lexer} parsed.
+     *
+     * @return An ordered list of tokens.
+     */
+    public List<String> getTokens() {
+        return Collections.unmodifiableList(tokens);
+    }
 
-	/**
-	 * Transforms a string into an ordered list of Lisp language tokens.
+    /**
+     * Transforms a string into an ordered list of Lisp language tokens.
 
-	 * @param s A string representing (presumably) a Lisp language statement or program.
-	 * @return An ordered list of Lisp language tokens extracted from the given string.
-	 */
-	private static List<String> tokenize(final String s) {
-		if (s.length() == 1) {
-			return Collections.singletonList(s);
-		}
+     * @param s A string representing (presumably) a Lisp language statement or program.
+     * @return An ordered list of Lisp language tokens extracted from the given string.
+     */
+    private static List<String> tokenize(final String s) {
+        if (s.length() == 1) {
+            return Collections.singletonList(s);
+        }
 
-		int i = 0;
-		final List<String> tokens = new ArrayList<>();
+        int i = 0;
+        final List<String> tokens = new ArrayList<>();
 
-		while (i < s.length()) {
-			int j = i + 1;
-			if (s.substring(i, j).matches(Grammar.LETTER) || s.substring(i, j).matches(Grammar.NUMERIC_LITERAL_START)) {
-				while (j < s.length() && (s.substring(i,j + 1).matches(Grammar.ALPHA_LITERAL) || s.substring(i, j + 1).matches(Grammar.NUMERIC_LITERAL))) {
-					j++;
-				}
-				tokens.add(s.substring(i,j));
-			} else if (s.substring(i, j).matches(Grammar.SYMBOL)) {
-				tokens.add(s.substring(i,j));
-			}
-			i = j;
-		}
-		return tokens;
-	}
+        while (i < s.length()) {
+            int j = i + 1;
+            if (s.substring(i, j).matches(Grammar.LETTER) || s.substring(i, j).matches(Grammar.NUMERIC_LITERAL_START)) {
+                while (j < s.length() &&
+                       (s.substring(i,j + 1).matches(Grammar.ALPHA_LITERAL) || s.substring(i, j + 1).matches(Grammar.NUMERIC_LITERAL))) {
+                    j++;
+                }
+                tokens.add(s.substring(i,j));
+            } else if (s.substring(i, j).matches(Grammar.SYMBOL)) {
+                tokens.add(s.substring(i,j));
+            }
+            i = j;
+        }
+        return tokens;
+    }
 }
