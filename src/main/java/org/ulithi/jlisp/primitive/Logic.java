d2dbdc72d9d978a0125c7850354a46d445c261fe
Joel Shepherd <joelshep@ix.netcom.com>
2024-11-09 21:28:23 -0800

    Logic - Add AND, OR and NOT functions

--- src/main/java/org/ulithi/jlisp/primitive/Logic.java
+++ src/main/java/org/ulithi/jlisp/primitive/Logic.java
@@ -2,8 +2,10 @@ package org.ulithi.jlisp.primitive;
 
 import org.ulithi.jlisp.core.AbstractSymbol;
 import org.ulithi.jlisp.core.Atom;
+import org.ulithi.jlisp.core.BindableFunction;
 import org.ulithi.jlisp.core.Binding;
 import org.ulithi.jlisp.core.BindingProvider;
+import org.ulithi.jlisp.core.Environment;
 import org.ulithi.jlisp.core.SExpression;
 
 import java.util.Arrays;
@@ -17,10 +19,100 @@ public class Logic implements BindingProvider {
     /** {@inheritDoc} */
     @Override
     public List<Binding> getBindings() {
-        return Arrays.asList(new Binding(new Logic.T()),
+        return Arrays.asList(new Binding(new Logic.AND()),
+                             new Binding(new Logic.NOT()),
+                             new Binding(new Logic.OR()),
+                             new Binding(new Logic.T()),
                              new Binding(new Logic.F()));
     }
 
+    /**
+     * Implements the LISP {@code AND} special function. Returns {@code NIL} if any argument
+     * evaluates to {@code NIL} or {@code F}. If every argument evaluates to a non-{@code NIL}
+     * value, returns the value of the last argument, or {@code T} if invoked without arguments.
+     * {@code AND} evaluates its arguments from left to right and short-circuits evaluation: once an
+     * argument which evaluates to {@code NIL} or {@code F} is encountered, no further arguments
+     * are evaluated.
+     */
+    public static class AND extends BindableFunction {
+        public AND() { super("AND"); }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean isSpecial() { return true; }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean isReentrant() { return true; }
+
+        /** {@inheritDoc} */
+        @Override
+        public SExpression apply(final SExpression sexp, final Environment env, final Eval eval) {
+            final Args args = Args.create(sexp);
+            SExpression value = Atom.T;
+
+            while (args.hasNext()) {
+                final SExpression arg = args.wantAny();
+                value = eval.eval(arg);
+                if (!t(value)) { return Atom.NIL; }
+            }
+
+            return value;
+        }
+    }
+
+    /**
+     * Implements the LISP {@code NOT} function, which returns {@code T} if its argument is NIL or
+     * an {@code Atom} that is equivalent to {@link F}, or {@code F} otherwise.
+     */
+    public static class NOT extends BindableFunction {
+        public NOT() { super("NOT"); }
+
+        /** {@inheritDoc} */
+        @Override
+        public SExpression apply(final SExpression sexp) {
+            final Args args = Args.create(sexp).expect(1);
+            return Atom.create(!t(args.wantAny()));
+        }
+    }
+
+    /**
+     * Implements the LISP {@code OR} special function. Returns {@code T} if any argument evaluates
+     * to {@code T}. If every argument evaluates to a{@code NIL} or {@code F} value, returns the
+     * value of the last argument, or {@code F} if invoked without arguments. {@code OR} evaluates
+     * its arguments from left to right and short-circuits evaluation: once an argument which
+     * evaluates to {@code T} is encountered, no further arguments are evaluated.
+     */
+    public static class OR extends BindableFunction {
+        public OR() { super("OR"); }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean isSpecial() { return true; }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean isReentrant() { return true; }
+
+        /** {@inheritDoc} */
+        @Override
+        public SExpression apply(final SExpression sexp, final Environment env, final Eval eval) {
+            final Args args = Args.create(sexp);
+            SExpression value = Atom.NIL;
+
+            while (args.hasNext()) {
+                final SExpression arg = args.wantAny();
+                value = eval.eval(arg);
+                if (t(value)) { return Atom.T; }
+            }
+
+            return value;
+        }
+    }
+
+    /**
+     * A symbol representing a Boolean "true" value.
+     */
     public static class T extends AbstractSymbol {
         public T() { super("T"); }
 
@@ -29,11 +121,25 @@ public class Logic implements BindingProvider {
         public SExpression eval() { return Atom.T; }
     }
 
+    /**
+     * A symbol representing a Boolean "false" value.
+     */
     public static class F extends AbstractSymbol {
         public F() { super("F"); }
 
         /** {@inheritDoc} */
         @Override
         public SExpression eval() { return Atom.F; }
+    }
+
+    /**
+     * Determines if the given {@link SExpression} represents a truth value: is either an
+     * {@link Atom} that is equivalent to a Boolean True (see {@link Atom#toB()} or a non-NIL
+     * {@link List}.
+     * @param sexp An {@link SExpression}.
+     * @return True if the given {@code SExpression} represents a truth value: false otherwise.
+     */
+    private static boolean t(final SExpression sexp) {
+        return sexp.isAtom() ? sexp.toAtom().toB() : !sexp.isNil();
     }
 }
--- src/test/java/org/ulithi/jlisp/test/primitive/LogicTestCase.java
+++ src/test/java/org/ulithi/jlisp/test/primitive/LogicTestCase.java
@@ -1,15 +1,129 @@
 package org.ulithi.jlisp.test.primitive;
 
 import org.junit.Test;
+import org.ulithi.jlisp.core.Atom;
+import org.ulithi.jlisp.core.SExpression;
+import org.ulithi.jlisp.test.suite.UnitTestUtilities;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.ulithi.jlisp.test.suite.UnitTestUtilities.eval;
+import static org.ulithi.jlisp.test.suite.UnitTestUtilities.newSession;
 
 /**
  * Unit tests for {@link org.ulithi.jlisp.primitive.Logic}.
  */
 public class LogicTestCase {
+
+    @Test
+    public void testSimpleAnd() {
+        assertTrue(eval("(AND (EQL 1 1) (EQL 2 2))").toAtom().toB());
+    }
+
+    @Test
+    public void testSimpleAndWithBooleanSymbols() {
+        assertTrue(eval("(AND T T T)").toAtom().toB());
+        assertFalse(eval("(AND T F T)").toAtom().toB());
+        assertFalse(eval("AND F F F").toAtom().toB());
+    }
+
+    @Test
+    public void testAndWithFalseFirstArgument() {
+        assertFalse(eval("(AND (NOT T) (NOT NIL))").toAtom().toB());
+    }
+
+    @Test
+    public void testAndWithFalseSecondArgument() {
+        assertFalse(eval("(AND (NOT F) (NOT T))").toAtom().toB());
+    }
+
+    @Test
+    public void testAndWithEmptyArgumentList() {
+        assertTrue(eval("(AND)").toAtom().toB());
+    }
+
+    @Test
+    public void testAndWithSingleArgument() {
+        assertTrue(eval("(AND (EQL 1 1))").toAtom().toB());
+        assertFalse(eval("(AND (EQL 1 2))").toAtom().toB());
+    }
+
+    @Test
+    public void testAndDoesShortcutEvaluation() {
+        final UnitTestUtilities.Session session = newSession();
+        session.eval("(SETQ A 5)");
+        final SExpression result = session.eval("(AND T F (SETQ A 6))");
+        assertFalse(result.toAtom().toB());
+        assertEquals(5, session.eval("A").toAtom().toI());
+    }
+
+    @Test
+    public void testNotOfTrue() {
+        assertEquals(Atom.F, eval("(NOT T)"));
+    }
+
+    @Test
+    public void testNotOfFalse() {
+        assertEquals(Atom.T, eval("(NOT F)"));
+    }
+
+    @Test
+    public void testNotOfNil() {
+        assertEquals(Atom.T, eval("(NOT NIL)"));
+        assertEquals(Atom.T, eval("(NOT ())"));
+        assertEquals(Atom.T, eval("(NOT '())"));
+    }
+
+    @Test
+    public void testNotOfTruthy() {
+        assertEquals(Atom.F, eval("(NOT (EQL 4 4))"));
+        assertEquals(Atom.T, eval("(NOT (EQL 4 3))"));
+        assertEquals(Atom.F, eval("(NOT '(A B C))"));
+    }
+
+
+    @Test
+    public void testSimpleOr() {
+        assertTrue(eval("(OR (EQL 1 0) (EQL 1 1))").toAtom().toB());
+    }
+
+    @Test
+    public void testSimpleORWithBooleanSymbols() {
+        assertTrue(eval("(OR T T T)").toAtom().toB());
+        assertTrue(eval("(OR F F T)").toAtom().toB());
+        assertFalse(eval("OR F F F").toAtom().toB());
+    }
+
+    @Test
+    public void testORWithFalseFirstArgument() {
+        assertTrue(eval("(OR (NOT T) (NOT NIL))").toAtom().toB());
+    }
+
+    @Test
+    public void testORWithFalseSecondArgument() {
+        assertTrue(eval("(OR (NOT F) (NOT T))").toAtom().toB());
+    }
+
+    @Test
+    public void testOrWithEmptyArgumentList() {
+        assertFalse(eval("(OR)").toAtom().toB());
+    }
+
+    @Test
+    public void testORWithSingleArgument() {
+        assertTrue(eval("(OR (EQL 1 1))").toAtom().toB());
+        assertFalse(eval("(OR (EQL 1 2))").toAtom().toB());
+    }
+
+    @Test
+    public void testOrDoesShortcutEvaluation() {
+        final UnitTestUtilities.Session session = newSession();
+        session.eval("(SETQ A 5)");
+        final SExpression result = session.eval("(OR F T (SETQ A 6))");
+        assertTrue(result.toAtom().toB());
+        assertEquals(5, session.eval("A").toAtom().toI());
+    }
 
     @Test
     public void testT() {
